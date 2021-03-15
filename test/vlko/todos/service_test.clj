606c95519b20b3b7b1d84a4f8e9197aa80207c38
Peter Vlk <petervlk86@gmail.com>
2021-03-15 16:09:25 +0100

    initial commit

---
+++ test/vlko/todos/service_test.clj
@@ -0,0 +1,7 @@
+(ns vlko.todos.service-test
+  (:require [clojure.test :refer :all]
+            [vlko.todos.service :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
