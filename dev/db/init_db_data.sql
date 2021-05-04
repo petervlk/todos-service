ce6feed8737dd713e39a5fa299049160755c505b
Peter Vlk <petervlk86@gmail.com>
2021-05-04 16:51:15 +0200

    remove obsolete db tables from DEV data

--- dev/db/init_db_data.sql
+++ dev/db/init_db_data.sql
@@ -7,41 +7,8 @@ CREATE TABLE todo_simple_items
     content VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE todo_lists
-(
-    id        UUID PRIMARY KEY             DEFAULT uuid_generate_v4(),
-    label     VARCHAR(255) UNIQUE NOT NULL,
-    timestamp TIMESTAMP           NOT NULL DEFAULT now()
-);
-
-CREATE TABLE todo_items
-(
-    id        UUID PRIMARY KEY             DEFAULT uuid_generate_v4(),
-    list_id   UUID                NOT NULL,
-    label     VARCHAR(255) UNIQUE NOT NULL,
-    completed BOOLEAN             NOT NULL DEFAULT FALSE,
-    timestamp TIMESTAMP           NOT NULL DEFAULT now(),
-    FOREIGN KEY (list_id)
-        REFERENCES todo_lists (id)
-);
-
 INSERT INTO todo_simple_items (label, content)
 VALUES ('system', 'use integrant');
 
 INSERT INTO todo_simple_items (label, content)
 VALUES ('db', 'use connection pool');
-
-INSERT INTO todo_lists (label)
-VALUES ('chores');
-
-INSERT INTO todo_lists (label)
-VALUES ('webapp');
-
-INSERT INTO todo_items (list_id, label)
-VALUES ((SELECT id FROM todo_lists WHERE label = 'webapp' LIMIT 1), 'configure DB');
-
-INSERT INTO todo_items (list_id, label)
-VALUES ((SELECT id FROM todo_lists WHERE label = 'chores' LIMIT 1), 'take out trash');
-
-INSERT INTO todo_items (list_id, label, completed)
-VALUES ((SELECT id FROM todo_lists WHERE label = 'chores' LIMIT 1), 'do the dishes', true );
