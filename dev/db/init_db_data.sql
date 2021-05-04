863f7d076ad00baf2974f5d8f473878f8f6d406c
Peter Vlk <petervlk86@gmail.com>
2021-05-04 14:24:19 +0200

    add new db table

--- dev/db/init_db_data.sql
+++ dev/db/init_db_data.sql
@@ -1,6 +1,12 @@
 CREATE
 EXTENSION IF NOT EXISTS "uuid-ossp";
 
+CREATE TABLE todo_simple_items
+(
+    label   VARCHAR(50) PRIMARY KEY NOT NULL,
+    content VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE todo_lists
 (
     id        UUID PRIMARY KEY             DEFAULT uuid_generate_v4(),
@@ -18,6 +24,12 @@ CREATE TABLE todo_items
     FOREIGN KEY (list_id)
         REFERENCES todo_lists (id)
 );
+
+INSERT INTO todo_simple_items (label, content)
+VALUES ('system', 'use integrant');
+
+INSERT INTO todo_simple_items (label, content)
+VALUES ('db', 'use connection pool');
 
 INSERT INTO todo_lists (label)
 VALUES ('chores');
